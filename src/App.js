932852160164948e632691c340a60777680d7295
braj011 <bhagyaraj01@gmail.com>
2018-11-09 11:49:32 +0000

    inital creation of front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Button  from '@material-ui/core/Button'
+// import InputBase from '@material-ui/core/InputBase';
+
 
 class App extends Component {
+
+  state = {
+    logged_in: false,
+    searchInput: ""
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+
+          <h1
             className="App-link"
-            href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-          </a>
+           Welcome to the source of real news
+          </h1>
+          <Button variant="contained" color="primary">Sign up</Button>
+          <br></br>
+          <Button variant="contained" color="primary">Log in</Button>
+          <form>
+              <input type="text" name="search" placeholder="Search..." />
+            <input type="submit" value="Submit" />
+          </form>
         </header>
       </div>
     );
